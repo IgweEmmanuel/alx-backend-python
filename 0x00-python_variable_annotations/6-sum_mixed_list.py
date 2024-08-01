5c1c3cc1ec50ff6da9740198e4a889c4e0e36474
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-01 22:32:27 +0000

    sum mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Complex types - mixed list
 from typing import List
 
 
-def sum_mixed_list(mxd_lst: List[int]) -> float:
+def sum_mixed_list(mxd_lst: List[int,float]) -> float:
     """
     Returns sum of mixed list
     """
