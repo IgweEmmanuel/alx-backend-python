7a1ca55227090a17737fb62ee211a87dcbbd9380
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-01 21:49:29 +0000

    floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import math
+"""floor of arguement"""
+
+
+def floor(n: float) -> int:
+    """function returns the floor of n
+    Args:
+        n - float number
+    """
+    return math.floor(n)
