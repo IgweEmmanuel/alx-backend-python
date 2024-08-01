6a79a6d62c20423cd740c29f3b7a643da78cf693
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-01 21:39:07 +0000

    this concatenates strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+"""This is concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """A function that concatenates two strings
+    Args:
+        str1 - string one
+        str2 - string two
+    """
+    return str1 + str2
