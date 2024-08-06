71ec7294993ffa8b0da78c61fcfa9253e6f6fc18
IgweEmmanuel <chigozieigweemmanuel@gmail.com>
2024-08-06 22:51:58 +0000

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A coroutine called async_generator that takes no arguments.
+"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """
+    async generator function
+    Return: returns random value asynchronously
+    """
+
+    for i in range(10):
+        ran = random.uniform(0, 10)
+        await asyncio.sleep(1)
+        yield ran
